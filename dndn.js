6d1bab49cffecb43b86fbef4cb3291b16f983869
Aboood13243 <49017354+Aboood13243@users.noreply.github.com>
2019-04-22 16:05:56 +0200

    Add files via upload

---
+++ dndn.js
@@ -0,0 +1,31 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+
+console.log("Welcome Again !");
+ 
+ client.on("guildMemberRemove", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+:kissing::heart::eyes: حيااكْ الله  فى سيرفرنا 
+╔╦╦╦═╦╗╔═╦═╦══╦═╗
+║║║║╩╣╚╣═╣║║║║║╩╣
+╚══╩═╩═╩═╩═╩╩╩╩═╝ 
+
+‏نقدم لك دعوة لسموك الراقيَ .
+‏لنحظى بجمال حضورك وتواجدك
+•بتنـورونـا الجميع :wink:
+•اي مشكله توآجهك ولا يهمك. عندك الاداره تفضل ونحل مشكلتك مع آي شخص بالسيرفر.:telephone_receiver:
+•عندك فكره موضوع تفضل شآركنا فيه:jack_o_lantern::gift:
+•واخيرا حييييياكم واكيد وجودكم هو اللي بيدعمنا وراح نكون الافضل.
+وصوت للرقم 4 حتى نسةي قعاليات و اشياء مراا راح تجب الناس اتمنى تدخل و تقرا القوانين جيدا
+شرفنا يا عسل:wink: :kissing_heart:
+
+https://discord.gg/AZ3sbZ
+joined info-selfie
+! Selfie.                              
+الدعووة خاصة لك ي قلبي ... [ ${member}  ]
+**`)
+
+}).catch(console.error)
+})
+client.login('NTU4NjQwNzk2OTQ2MDA2MDI0.XL3Jyg.y5y49cFbDEJxCVqqbuwv14HcMn8');
